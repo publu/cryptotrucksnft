1d01146cc66998fedc0add15d5b67fc93275c691
Pablo S Peillard <Pjs130130@utdallas.edu>
2021-02-28 01:15:08 -0800

    referral pls

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ASSET_URL =
-  "https://opensea.io/assets/cryptotrucks"
+  "https://opensea.io/assets/cryptotrucks?ref=0x2AB4acE5A6d3CCB06c827a8cf6839CAf21bD023f"
   ;
 
 export enum EmbedTheme {
